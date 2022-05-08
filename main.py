885bba1a1738a6055e755c27f2976fab95bf18df
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-05-08 07:07:55 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.py
@@ -0,0 +1,62 @@
+radio.set_group(69)
+radio.set_transmit_power(7)
+radio.set_transmit_serial_number(True)
+my_serial = control.device_serial_number()
+
+name= "vote"
+name= "send"
+number= 1
+value= 0
+send_message = True
+list_of_votes = [0,0,0,0]
+
+def dissable_client():
+    if name == "send" and number == 0:
+        radio.send_value("send", 1)
+    else:
+        radio.send_value("send", 0)
+input.on_pin_pressed(TouchPin.P0, dissable_client)
+
+def voting_A():
+    global send_message
+    basic.show_string("A")
+    voted(0)
+    basic.pause(500)
+    basic.clear_screen()
+input.on_button_pressed(Button.A, voting_A)
+
+def voting_B():
+    global send_message
+    basic.show_string("B")
+    voted(1)
+    basic.pause(500)
+    basic.clear_screen()
+input.on_button_pressed(Button.B, voting_B)
+
+def voting_C():
+    global send_message
+    basic.show_string("C")
+    voted(2)
+    basic.pause(500)
+    basic.clear_screen()
+input.on_logo_event(TouchButtonEvent.PRESSED, voting_C)
+
+def voting_D():
+    global send_message
+    basic.show_string("D")
+    voted(3)
+    basic.pause(500)
+    basic.clear_screen()
+input.on_pin_pressed(TouchPin.P2, voting_D)
+
+def voted(data):
+    while send_message == True:
+        global list_of_votes
+        radio.send_value("vote", data)
+        break
+
+def on_received_value(name, value):
+    basic.show_number(value)
+    list_of_votes[value] +=1
+    print(list_of_votes)
+radio.on_received_value(on_received_value)
---
+++ main.ts
@@ -0,0 +1,59 @@
+radio.setGroup(69)
+radio.setTransmitPower(7)
+radio.setTransmitSerialNumber(true)
+let my_serial = control.deviceSerialNumber()
+let name = "vote"
+name = "send"
+let number = 1
+let value = 0
+let send_message = true
+let list_of_votes = [0, 0, 0, 0]
+input.onPinPressed(TouchPin.P0, function dissable_client() {
+    if (name == "send" && number == 0) {
+        radio.sendValue("send", 1)
+    } else {
+        radio.sendValue("send", 0)
+    }
+    
+})
+input.onButtonPressed(Button.A, function voting_A() {
+    
+    basic.showString("A")
+    voted(0)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function voting_B() {
+    
+    basic.showString("B")
+    voted(1)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function voting_C() {
+    
+    basic.showString("C")
+    voted(2)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function voting_D() {
+    
+    basic.showString("D")
+    voted(3)
+    basic.pause(500)
+    basic.clearScreen()
+})
+function voted(data: number) {
+    while (send_message == true) {
+        
+        radio.sendValue("vote", data)
+        break
+    }
+}
+
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    basic.showNumber(value)
+    list_of_votes[value] += 1
+    console.log(list_of_votes)
+})
