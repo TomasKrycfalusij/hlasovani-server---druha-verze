84b9727eb97fa71ded9b7246d3f75c0b331c579a
Tomáš Kryčfalušij <91247418+TomasKrycfalusij@users.noreply.github.com>
2022-05-08 17:29:02 +0200

    Aktualizovat main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # ÚVOD
 # 1. Zmáčknutí A/B/dotykového tlačítka/pinu 2 se odešle hlas
-# 2. V konzoli to vypíše váš aktuální hlas a sériové číslo microbitu
-# 3. Vypíše všechny vaše předešlé hlasy (budou uloženy v listu)
-# 4. Samotné čísílko mezi hlasy určuje o kolikátou pozici v listu hlasů se jedná
-# 5. Nepřišel jsem na způsob jak na detekci sériového čísla v listu,
+# 2. Pinem 0 se nastaví přijímání hlasů
+# 3. V konzoli to vypíše váš aktuální hlas a sériové číslo microbitu
+# 4. Vypíše všechny vaše předešlé hlasy (budou uloženy v listu)
+# 5. Samotné čísílko mezi hlasy určuje o kolikátou pozici v listu hlasů se jedná
+# 6. Nepřišel jsem na způsob jak na detekci sériového čísla v listu,
 #    přesněji pozice a jak funduje přiřazování v takovém stylu listu
+# 7. UPOZORNĚNÍ - Horní microbit je ten hlavní, proto je možné, že ikdyž deaktivujete hlasování
+#    na jednom, druhý bude stále moci přijímat hlasy. Proto pro odesílání hlasu používejte "spodní"
 
 radio.set_group(69)
 radio.set_transmit_power(7)
@@ -17,66 +20,67 @@ name = "send"
 number = 1
 value = 0
 posuvnik_1 = 1
-send_message = True
+receive_message = True
 list_of_votes = [{"serial": my_serial, "volba": 0}]
 
+#ZAPNUTÍ A VYPNUTÍ MOŽNOSTI HLASOVAT
 def zapinani_hlasovani():
-    global send_message
-    if send_message == True:
-        send_message = False
+    global receive_message
+    if receive_message == True:
+        global receive_message
+        receive_message = False
         basic.show_icon(IconNames.NO)
     else:
-        send_message = True
+        global receive_message
+        receive_message = True
         basic.show_icon(IconNames.YES)
+    basic.pause(500)
+    basic.clear_screen()
 input.on_pin_pressed(TouchPin.P0, zapinani_hlasovani)
 
 #HLASOVANI CISEL
 def voting_A():
-    global send_message
     voted(0)
     basic.show_string("A")
     basic.clear_screen()
 input.on_button_pressed(Button.A, voting_A)
 def voting_B():
-    global send_message
     voted(1)
     basic.show_string("B")
     basic.clear_screen()
 input.on_button_pressed(Button.B, voting_B)
 def voting_C():
-    global send_message
     voted(2)
     basic.show_string("C")
     basic.clear_screen()
 input.on_logo_event(TouchButtonEvent.PRESSED, voting_C)
 def voting_D():
-    global send_message
     voted(3)
     basic.show_string("D")
     basic.clear_screen()
 input.on_pin_pressed(TouchPin.P2, voting_D)
 #----------------------------------------------
+
 #ODESLÁNÍ HLASU
 def voted(data):
-    while send_message == True:
-        global list_of_votes
-        radio.send_value(str(my_serial), data)
-        break
+    global list_of_votes
+    radio.send_value(str(my_serial), data)
 
 #PŘIJMUTÍ HLASU
 def on_received_value(name, value):
     global list_of_votes, posuvnik_1
-    basic.show_number(value)
-    list_of_votes.append([{"serial": int(name), "volba": value}][0])
-    print("Toto byl tvůj hlas")
-    print([{"serial": int(name), "volba": value}][0])
-    posuvnik_2 = 0
-    print("Toto je list tvých minulých hlasů")
-    for i in range(posuvnik_1):
-        print(posuvnik_2)
-        print(list_of_votes[posuvnik_2])
-        posuvnik_2 += 1
-    posuvnik_1 += 1
-    basic.pause(500)
-    basic.clear_screen()
+    if receive_message == True:
+        basic.show_number(value)
+        list_of_votes.append([{"serial": int(name), "volba": value}][0])
+        print("Toto byl tvůj hlas")
+        print([{"serial": int(name), "volba": value}][0])
+        posuvnik_2 = 0
+        print("Toto je list tvých minulých hlasů")
+        for i in range(posuvnik_1):
+            print(posuvnik_2)
+            print(list_of_votes[posuvnik_2])
+            posuvnik_2 += 1
+        posuvnik_1 += 1
+        basic.pause(500)
+        basic.clear_screen()
 radio.on_received_value(on_received_value)
--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 //  ÚVOD
 //  1. Zmáčknutí A/B/dotykového tlačítka/pinu 2 se odešle hlas
-//  2. V konzoli to vypíše váš aktuální hlas a sériové číslo microbitu
-//  3. Vypíše všechny vaše předešlé hlasy (budou uloženy v listu)
-//  4. Samotné čísílko mezi hlasy určuje o kolikátou pozici v listu hlasů se jedná
-//  5. Nepřišel jsem na způsob jak na detekci sériového čísla v listu,
+//  2. Pinem 0 se nastaví přijímání hlasů
+//  3. V konzoli to vypíše váš aktuální hlas a sériové číslo microbitu
+//  4. Vypíše všechny vaše předešlé hlasy (budou uloženy v listu)
+//  5. Samotné čísílko mezi hlasy určuje o kolikátou pozici v listu hlasů se jedná
+//  6. Nepřišel jsem na způsob jak na detekci sériového čísla v listu,
 //     přesněji pozice a jak funduje přiřazování v takovém stylu listu
+//  7. UPOZORNĚNÍ - Horní microbit je ten hlavní, proto je možné, že ikdyž deaktivujete hlasování
+//     na jednom, druhý bude stále moci přijímat hlasy. Proto pro odesílání hlasu používejte "spodní"
 radio.setGroup(69)
 radio.setTransmitPower(7)
 radio.setTransmitSerialNumber(true)
@@ -15,44 +18,45 @@ name = "send"
 let number = 1
 let value = 0
 let posuvnik_1 = 1
-let send_message = true
+let receive_message = true
 let list_of_votes = [ {
     "serial" : my_serial,
     "volba" : 0,
 }
 ]
+// ZAPNUTÍ A VYPNUTÍ MOŽNOSTI HLASOVAT
 input.onPinPressed(TouchPin.P0, function zapinani_hlasovani() {
     
-    if (send_message == true) {
-        send_message = false
+    if (receive_message == true) {
+        
+        receive_message = false
         basic.showIcon(IconNames.No)
     } else {
-        send_message = true
+        
+        receive_message = true
         basic.showIcon(IconNames.Yes)
     }
     
+    basic.pause(500)
+    basic.clearScreen()
 })
 // HLASOVANI CISEL
 input.onButtonPressed(Button.A, function voting_A() {
-    
     voted(0)
     basic.showString("A")
     basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function voting_B() {
-    
     voted(1)
     basic.showString("B")
     basic.clearScreen()
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function voting_C() {
-    
     voted(2)
     basic.showString("C")
     basic.clearScreen()
 })
 input.onPinPressed(TouchPin.P2, function voting_D() {
-    
     voted(3)
     basic.showString("D")
     basic.clearScreen()
@@ -60,36 +64,37 @@ input.onPinPressed(TouchPin.P2, function voting_D() {
 // ----------------------------------------------
 // ODESLÁNÍ HLASU
 function voted(data: number) {
-    while (send_message == true) {
-        
-        radio.sendValue("" + my_serial, data)
-        break
-    }
+    
+    radio.sendValue("" + my_serial, data)
 }
 
 // PŘIJMUTÍ HLASU
 radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    let posuvnik_2: number;
     
-    basic.showNumber(value)
-    list_of_votes.push([ {
-        "serial" : parseInt(name),
-        "volba" : value,
+    if (receive_message == true) {
+        basic.showNumber(value)
+        list_of_votes.push([ {
+            "serial" : parseInt(name),
+            "volba" : value,
+        }
+        ][0])
+        console.log("Toto byl tvůj hlas")
+        console.log([ {
+            "serial" : parseInt(name),
+            "volba" : value,
+        }
+        ][0])
+        posuvnik_2 = 0
+        console.log("Toto je list tvých minulých hlasů")
+        for (let i = 0; i < posuvnik_1; i++) {
+            console.log(posuvnik_2)
+            console.log(list_of_votes[posuvnik_2])
+            posuvnik_2 += 1
+        }
+        posuvnik_1 += 1
+        basic.pause(500)
+        basic.clearScreen()
     }
-    ][0])
-    console.log("Toto byl tvůj hlas")
-    console.log([ {
-        "serial" : parseInt(name),
-        "volba" : value,
-    }
-    ][0])
-    let posuvnik_2 = 0
-    console.log("Toto je list tvých minulých hlasů")
-    for (let i = 0; i < posuvnik_1; i++) {
-        console.log(posuvnik_2)
-        console.log(list_of_votes[posuvnik_2])
-        posuvnik_2 += 1
-    }
-    posuvnik_1 += 1
-    basic.pause(500)
-    basic.clearScreen()
+    
 })
